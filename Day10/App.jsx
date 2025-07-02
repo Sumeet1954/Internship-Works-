9aa19817231ebec263b58d3419cd098a04f196e1
Sumeet1954 <thesumeetfacts@gmail.com>
2025-07-02 21:53:57 +0530

    Add files via upload

---
+++ Day10/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/header.jsx'
+import Section1 from './components/Section1.jsx'
+
+function App() {
+
+  return (
+    <>
+    <Header />
+    <Section1 />
+    </>
+  )
+}
+
+export default App
