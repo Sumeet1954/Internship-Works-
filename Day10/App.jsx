d4062f84add57b52273d561cccb6336b4f019a42
Sumeet1954 <thesumeetfacts@gmail.com>
2025-07-04 22:06:31 +0530

    Add files via upload

--- Day10/App.jsx
+++ Day10/App.jsx
@@ -1,6 +1,10 @@
 import './App.css'
 import Header from './components/header.jsx'
 import Section1 from './components/Section1.jsx'
+import Section2 from './components/Section2.jsx'
+import Section4 from './components/Section4.jsx'
+import Section5 from './components/Section5.jsx'
+import Footer from './components/footer.jsx'
 
 function App() {
 
@@ -8,6 +12,10 @@ function App() {
     <>
     <Header />
     <Section1 />
+    <Section2/>
+    <Section4/>
+    <Section5/>
+    <Footer/>
     </>
   )
 }
