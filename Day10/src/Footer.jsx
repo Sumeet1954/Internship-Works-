2a67e64bb4036c8002b5e8520cf12f36a2dbd18d
Sumeet1954 <thesumeetfacts@gmail.com>
2025-07-08 12:10:42 +0530

    Add files via upload

--- Day10/src/Footer.jsx
+++ Day10/src/Footer.jsx
@@ -7,41 +7,44 @@ const Footer = () => {
           <div className="div-ft">
             <img src="js1.png" alt="Jetsetter Logo" class="logo" />
             <h6>More of Us </h6>
-            <ul>
-              <li>JetSetter</li>
-              <li>JetMart</li>
-            </ul>
+            <div>
+              <a>JetSetter</a>
+              <a>JetMart</a>
+            </div>
           </div>
           <div className="div-ft">
             <h6>For Restaurants</h6>
-            <ul>
-              <li>Partner With Us</li>
-              <li>Apps For You</li>
-            </ul>
+            <div>
+              <a>Partner With Us</a>
+              <a>Apps For You</a>
+            </div>
           </div>
           <div className="div-ft">
-            <h6>For Delivery Partners</h6>
-            <ul>
-              <li>Partner With Us</li>
-              <li>Apps For You</li>
-            </ul>
+            <h6>For Deavery Partners</h6>
+            <div>
+              <a>Partner With Us</a>
+              <a>Apps For You</a>
+            </div>
           </div>
           <div className="div-ft">
             <h6>Learn More</h6>
-            <ul>
-              <li>Privacy Policy</li>
-              <li>Terms & Condition</li>
-              <li>Refund & Cancellation</li>
-              <li>Return Policies</li>
-              <li>Shipping Policies</li>
-            </ul>
+            <div>
+              <a>Privacy Poacy</a>
+              <a>Terms & Condition</a>
+              <a>Refund & Cancellation</a>
+              <a>Return Poacies</a>
+              <a>Shipping Poacies</a>
+            </div>
           </div>
           <div className="div-ft">
-            <h6>Social Links</h6>
+            <h6>Social links</h6>
             <span></span>
           </div>
         </div>
-        <div className="footer-bootom"></div>
+        <div className="footer-bottom">
+          <hr></hr>
+          <p>By continuing past this page, you agree to our Terms of Service, Cookie Policy, Privacy Policy and Content Policies. All trademarks are properties of their respective owners 2025 © Jetsetetr Ltd. All rights reserved.</p>
+        </div>
       </footer>
     </>
   );
--- Day10/src/Section1.jsx
+++ Day10/src/Section1.jsx
@@ -6,33 +6,36 @@ export default function Section1() {
         <div className="section1__content">
           <h2>Better food for{<br />}more people</h2>
           <p>
-            For over a decade, we’ve enabled our{<br />}customers to discover
+            For over a decade, we’ve enabled {<br />}customers to discover
             new tastes,{<br />}delivered right to their doorstep
           </p>
           <div className="section1-card">
             <div className="col1">
+            <div>
               <h3>999+</h3>
               <p>Restaurents</p>
             </div>
-            <div>
+            
               {" "}
               <img src="https://jetsetterindia.com/assets/front_end/classic/images/countImg1.webp"></img>
             </div>
 
             <div className="col1">
+            <div>
               <h3>99+</h3>
               <p>Cities</p>
             </div>
-            <div>
+            
               
               <img src="https://jetsetterindia.com/assets/front_end/classic/images/countImg2.png"></img>
             </div>
 
             <div className="col1">
+            <div>
               <h3>1 Million+</h3>
               <p>order delivered</p>
             </div>
-            <div>
+            
               <img src="https://jetsetterindia.com/assets/front_end/classic/images/countImg3.png"></img>
             </div>
           </div>
