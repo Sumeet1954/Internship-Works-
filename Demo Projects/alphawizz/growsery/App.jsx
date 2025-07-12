cce066254c47afa69012a26c6aeda745833c33e4
Sumeet1954 <thesumeetfacts@gmail.com>
2025-07-12 22:53:28 +0530

    Add files via upload

---
+++ Demo Projects/alphawizz/growsery/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/Header'
+import Section1 from './components/Section1'
+import Footer from './components/Footer'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+    </>
+  )
+}
+
+export default App
